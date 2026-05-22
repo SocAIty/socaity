39b40a6d7f4dbf64a9d7b3ca3ba712197f99189e
K4rlosReyes <carlosreyesml18@gmail.com>
2026-05-22 11:18:48 +0200

    test: use latest frontend website

--- socaity/core/credentials.py
+++ socaity/core/credentials.py
@@ -10,7 +10,7 @@ from pathlib import Path
 from typing import Any, Optional
 
 DEFAULT_BACKEND_URL = "https://webapi.socaity.ai"
-DEFAULT_FRONTEND_URL = "http://localhost:3000"
+DEFAULT_FRONTEND_URL = "https://wonderful-wave-0a392ca03.2.azurestaticapps.net/"
 
 _LEGACY_TOKEN_FILE = Path.home() / ".apipod" / "token"
 
